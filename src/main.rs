518dfc9905216a5a954b51217cb28c20b21bb4d3
Thi Dinh <vietthidinh2001@gmail.com>
2021-12-24 14:00:40 +0200

    feat: implement reading file with command line arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Query: {}", query);
+    println!("File: {}", filename);
+
+    let content =
+        fs::read_to_string(filename).expect("Something went wrong with reading the file.");
+
+    println!("Content: {}", content);
 }
