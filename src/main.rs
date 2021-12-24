7b413384259848e5779a03ee69e6b01d6ca30918
Thi Dinh <vietthidinh2001@gmail.com>
2021-12-24 19:25:03 +0200

    chore: extract logic into functions and add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,42 @@
-use std::{env, fs};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
-    let query = &args[1];
-    let filename = &args[2];
 
-    println!("Query: {}", query);
-    println!("File: {}", filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let content =
-        fs::read_to_string(filename).expect("Something went wrong with reading the file.");
+    println!("Query: {}", config.query);
+    println!("File: {}", config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.filename)?;
 
     println!("Content: {}", content);
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
 }
