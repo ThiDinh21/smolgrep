a9feb23e836e500c94f9200ad063b5f0523f78a9
Thi Dinh <vietthidinh2001@gmail.com>
2021-12-24 20:46:29 +0200

    test: write sample tests for Config::new()

--- src/lib.rs
+++ src/lib.rs
@@ -23,3 +23,37 @@ impl Config {
         Ok(Config { query, filename })
     }
 }
+
+#[cfg(test)]
+mod test_config {
+    use crate::Config;
+
+    #[test]
+    fn no_arg() {
+        assert!(Config::new(&[]).is_err(), "No arg should return an error.");
+    }
+
+    #[test]
+    fn one_arg() {
+        assert!(
+            Config::new(&[String::from("Hello, world!")]).is_err(),
+            "One arg should return an error."
+        );
+    }
+
+    #[test]
+    fn wrong_filename() {
+        assert!(
+            Config::new(&[String::from("Hello, world!"), String::from("Stuffs")]).is_err(),
+            "Filename that are different than poem.txt should return an error."
+        );
+    }
+
+    #[test]
+    fn valid_input() {
+        assert!(
+            Config::new(&[String::from("Hello, world!"), String::from("poem.txt")]).is_err(),
+            "Valid inputs shouldn't return an error."
+        );
+    }
+}
